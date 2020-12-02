9b90250a4ffe3a38a6110f45bc2dfd3c13a5dcf5
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2020-12-02 12:47:29 -0500

    new changes

--- src/Atoms/Translation.js
+++ src/Atoms/Translation.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import palabras from '../Assets/palabras.json';
 import CircularLoader from './CircularLoader';
+import { makeStyles } from '@material-ui/core/styles';
+
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
